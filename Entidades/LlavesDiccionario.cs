ac60bc9a0caa7887b7ac941ab66cf5e39b2e18f4
Alberto <admin@darksnakegames.com>
2020-09-28 21:45:30 +0200

    Constantes LlavesDiccionario

--- Entidades/LlavesDiccionario.cs
+++ Entidades/LlavesDiccionario.cs
@@ -3,5 +3,15 @@ namespace Project_Carthage.Entidades
 {
     public struct LlavesDiccionario
     {
+
+        public const string CURSOS = "Cursos";
+
+        public const string ALUMNOS = "Alumnos";
+
+        public const string Asignaturas = "Asignaturas";
+
+        public const string ESCUELA = "Escuela";
+
+        public const string EVALUACIONES = "Evaluaciones";
     }
 }
