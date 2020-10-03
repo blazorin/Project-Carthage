e55049cc0cf42f1ac252faef37ff0d2b4f19157e
Alberto <admin@darksnakegames.com>
2020-10-03 17:18:26 +0200

    Creación Reporteador

---
+++ App/Reporteador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Project_Carthage.Entidades;
+
+namespace Project_Carthage.App
+{
+    public class Reporteador
+    {
+        Dictionary<LLaveDiccionario, IEnumerable<ParentEntity>> _diccionario;
+
+        public Reporteador()
+        {
+        }
+    }
+}
